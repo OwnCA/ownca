820e1f489793a8c95869e8d6b260d78ffc27cc37
Kairo Araujo <kairo@kairo.eti.br>
2021-06-19 08:30:30 +0200

    Release 0.2.1
    Added support to Windows

--- ownca/__version__.py
+++ ownca/__version__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Copyright (c) 2018-2020 Kairo de Araujo
+Copyright (c) 2018-2021 Kairo de Araujo
 """
 
 __title__ = "OwnCA"
 __description__ = "Python Own Certificate Authority"
 __url__ = "https://github.com/OwnCA/ownca"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Kairo de Araujo"
 __author_email__ = "kairo@dearaujo.nl"
 __license__ = "Apache 2.0"
-__copyright__ = "2018-2020, Kairo de Araujo"
+__copyright__ = "2018-2021, Kairo de Araujo"
