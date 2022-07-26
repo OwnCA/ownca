f5604fc13679e871c3599a801250aa123dbd5020
KAUTH <konpap1996@yahoo.com>
2022-07-26 20:32:31 +0200

    Migrate setup.py to pyproject.toml
    Modern Python packages can contain a pyproject.toml file,
    first introduced in PEP 518 and later expanded in PEP 517,
    PEP 621 and PEP 660.
    With this commit we prepare the pyproject.toml using
    setuptools as a build-backend.
    
    We do not remove setup.py and setup.cfg yet, since pip
    still requires those files to support editable installs
    (PEP 660). https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html#
    
    We also use the Build package to build our package
    before uploading to PyPI.
    (while building we get a message that:
    _BetaConfiguration: Support for `[tool.setuptools]` in
    `pyproject.toml` is still *beta*.)
    
    Fixes #72

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+# pip still might require a setup.py file to support ...
+# editable installs (PEP 660)
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+# keep this file until it does
+
 #!/usr/bin/env python
 import os
 import sys
